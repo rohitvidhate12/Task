e408d2f0266ae7e2b5ca3f3b3e2725d0b216dde2
Vidhate Rohit <rohitvidhate12@gmail.com>
2023-05-09 14:43:48 +0530

    EmpMaster

---
+++ src/components/employees/employeeList.js
@@ -0,0 +1,100 @@
+import React from "react";
+import MUIDataTable from "mui-datatables";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Box, Button, IconButton } from "@mui/material";
+import { designationList } from "./employeeForm";
+
+const EmployeeList = ({ data, onAdd, onEdit, onDelete }) => {
+  const columns = [
+    {
+      label: "Employee No",
+      name: "employeeNo",
+      options: {
+        sort: false,
+        filter: true,
+      },
+    },
+    {
+      label: "Employee Name",
+      name: "employeeName",
+      options: {
+        sort: true,
+        filter: true,
+        customBodyRenderLite: (index) => {
+          const user = data[index];
+          return `${user?.firstName} ${user?.lastName}`;
+        },
+      },
+    },
+    {
+      label: "Designation",
+      name: "designationId",
+      options: {
+        sort: false,
+        filter: true,
+        customBodyRenderLite: (index) => {
+          const employee = data[index];
+          const designation = designationList.find(
+            (d) => d.designationId === employee.designationId
+          ).designation;
+          return <span>{designation}</span>;
+        },
+      },
+    },
+    {
+      label: "Date of Joining",
+      name: "date",
+      options: {
+        sort: false,
+        filter: false,
+        customBodyRenderLite: (index) => {
+          const emppObj = data[index];
+          return new Date(emppObj.date)?.toLocaleDateString();
+        },
+      },
+    },
+    {
+      label: "Salary (PA)",
+      name: "salary",
+      options: {
+        sort: false,
+        filter: false,
+      },
+    },
+    {
+      label: "Action",
+      name: "action",
+      options: {
+        sort: false,
+        filter: false,
+        customBodyRenderLite: (index) => {
+          const user = data[index];
+          return (
+            <>
+              <IconButton color="primary" onClick={() => onEdit(user)}>
+                <EditIcon />
+              </IconButton>
+              <IconButton color="error" onClick={() => onDelete(user)}>
+                <DeleteIcon />
+              </IconButton>
+            </>
+          );
+        },
+      },
+    },
+  ];
+
+  return (
+    <>
+      <Box>
+        <Button variant="contained" onClick={onAdd}>
+          Add ++
+        </Button>
+        <MUIDataTable data={data} columns={columns} />
+      </Box>
+    </>
+  );
+};
+
+export default EmployeeList;
